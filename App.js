0ba8108f8a6f695135d3d5be66c076c9f8098a20
vinesh113 <158165603+vinesh113@users.noreply.github.com>
2025-02-10 10:27:03 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,48 @@
+return
+div style={{backgroudColor:theme=='light'?'black','white',}}
+<h1
+// import React, { useState } from 'react';
+
+// function App() {
+//   const [count, setCount] = useState(0);
+
+//   return (
+//     <div>
+//       <h1>{count}</h1>
+//       <button onClick={() => setCount(count + 1)}>Increment</button>
+//       <button onClick={() => setCount(count - 1)}>Decrement</button>
+//     </div>
+//   );
+// }
+
+// export default App;
+
+
+// import React from 'react';
+// import Greeting from './Greeting';  // Make sure the file name matches the correct casing
+// import './App.css';
+
+// function App() {
+//   const isLoggedIn = false; 
+
+//   return (
+//     <div className="App">
+//       <Greeting isLoggedIn={isLoggedIn} />
+//     </div>
+//   );
+// }
+
+// export default App;
+
+
+// // const Welcome = () => <h1 className='message'>Hello Sai</h1>;
+
+// // function App() {
+// //   return (
+// //     <div className='App'>
+// //       <Welcome />
+// //     </div>
+// //   );
+// // }
+
+// //  export default Welcome;
